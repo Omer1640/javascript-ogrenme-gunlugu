2bfa653575d31fc253f9733fae695b020e1c21a3
Ömer Kurnaz <omerbey162540@gmail.com>
2026-06-12 20:30:50 +0300

    chore: Aritmetik operatörler bölümünü bitirdim.

---
+++ 04-Operatorler/kodlar.js
@@ -0,0 +1,93 @@
+// * Operatörler
+
+// 12 Haziran 2026 - Enes Bayram Kursu - Bölüm 9  - Ders 68
+
+// * Aritmetik Operatörler
+
+// ! Not:  =  atama operatörüdür 5 = 5 eşittir değildir.
+
+// let sayi = 5; 5 sayısını sayi değişkenine atamış oldum.
+
+// ? Aritmetik Operatörler şunlardır
+/*
+ "+": Sayısal ifadelerde toplama işlemi , string ifadelerin olduğu yerde birleştirme  
+
+ "-": Sayısal ifadelerde çıkarma işlemi yapar
+
+ "*": Sayısal ifadelerde çarpma işlemi yapar
+
+ "/": Sayısal ifadelerde bölme işlemi yapar
+
+ "%": Mod alma (bölünen sayının kalanını bulur)
+
+ "++": Sayıları bir bir artırır
+
+ "--": Sayıları bir bir azaltır
+
+"**": üs almada kullanılır
+*/
+
+// *  "+" Operatörü
+
+// Sayısal ifadelerde kullanımı
+// let sayi1 = 10;
+// let sayi2 = 20;
+// let toplam = sayi1 + sayi2;
+// console.log(toplam);
+
+// String ifadelerdeki birleştirme görevi
+/*
+let sayi1 = 200;
+let sayi2 = 600;
+console.log("Toplam Sonuç:" + (sayi1 + sayi2));  Buradaki Çıktı string ifade ile number ifadeyi birleştirdi.
+*/
+
+// *  "-" Operatörü
+// Sayısal ifadelerde çıkarma işlemi yapar
+// let kazanc = 80.0;
+// let borc = 25.0;
+// let kalan = kazanc - borc;
+// console.log("Geriye kalan toplam para:" + " " + kalan);
+
+// *  "*" Operatörü
+// Sayısal ifadelerde çarpma işlemi yapar
+// console.log(10 * 50);
+// let a = 25;
+// let b = 16;
+// let carpim = a * b;
+// console.log(carpim);
+
+// *  "/"  Operatörü
+// Sayısal ifadelerde bölme işlemi yapar
+// let sayi1 = 77;
+// let sayi2 = 13;
+// console.log((sayi1 + sayi2) / 2);
+
+// *  "%" (Mod Alma) Operatörü
+// iki tane bölünen sayının kalanını bulur
+// let sayi1 = 34;
+// let sayi2 = 16;
+// console.log(sayi1 % sayi2);
+// *  "++"  Operatörü
+// sayıları birer birer artırır
+// let a = 12;
+// a++;
+// a++;
+// a++;
+// a++;
+// console.log(a);
+
+// *  "--"  Operatörü
+// sayıları birer birer azaltır
+// let a = 28;
+// a--;
+// a--;
+// a--;
+// console.log(a);
+
+// * (**) Operatörü
+// Sayısal ifadelerde üs alma olarak kullanılır
+
+// let a = 8;
+// let b = 4;
+// console.log(a ** b);
---
+++ 04-Operatorler/notlar.md
@@ -0,0 +1,20 @@
+# 💻 Operatörlere Giriş
+
+## 📄 Aritmetik Operatörler
+
+**Tarih:** 12 Haziran 2026
+
+**Kaynak:** Enes Bayram Kursu - Bölüm 9 - Ders 69
+
+---
+
+### Ne Öğrendim ?
+- (=) bu işaret atama operatörüdür eşittir işareti ile çok karışır örnek = let a = 10 ; burda a değişkenine 10 sayısını atadık , let a == 10 ; burda a değişeni 10 eşit. 
+- 8 tane aritmetik operatör bulunmaktadır.
+- (+) Sayısal ifadelerde toplama işlemi yapar string ve number bir ifadenin arasında birleştirme görevi görür örnek: 10 + "20" 1020 olur.
+- (-),(*),(/) çıkarma , çarpma , bölme işlemleri yaparlar.
+- (%) mod alma işlemi yapar yani iki sayının bölümünden kalanını bulmak için kullanılır.
+- (++),(--) değerlerimizi birer birer artırır ve azaltır örnek: let a = 10; a++; = 11 , a--; = 9 olur.
+- (**) bir sayının üssünü alır.
+
+---
